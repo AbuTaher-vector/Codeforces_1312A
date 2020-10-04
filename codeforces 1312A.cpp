4a7972203f6a1a2816384a63fdcd0024a0af1fa7
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2020-10-04 12:38:08 +0600

    Add files via upload

---
+++ codeforces 1312A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int n,m;
+        cin>>n>>m;
+        if(n%m==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
